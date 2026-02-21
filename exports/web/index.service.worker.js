b74dca156f9d1de8f9bc60c90b9eb25b9e678c9e
Luca Sain <luca.sain@outlook.com>
2026-02-21 01:06:48 -0300

    feat: add initial web export for Godot project, including service worker and HTML.

--- exports/web/index.service.worker.js
+++ exports/web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771639860|14874702';
+const CACHE_VERSION = '1771646779|68939682';
 /** @type {string} */
 const CACHE_PREFIX = 'Luz en Once-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
